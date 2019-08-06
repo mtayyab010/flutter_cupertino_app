b246e61e77fca32737a2a993a06966ff5663b2d2
hiyongrui <172808H@mymail.nyp.edu.sg>
2019-08-06 20:12:46 +0800

    add product search

---
+++ lib/search_bar.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/widgets.dart';
+import 'styles.dart';
+
+//this class handles actual search in product list
+class SearchBar extends StatelessWidget {
+  const SearchBar({
+    @required this.controller,
+    @required this.focusNode,
+  });
+
+  final TextEditingController controller;
+  final FocusNode focusNode;
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        color: Styles.searchBackground,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 4,
+          vertical: 8,
+        ),
+        child: Row(
+          children: [
+            const Icon(CupertinoIcons.search, color: Styles.searchIconColor),
+            Expanded(
+              child: CupertinoTextField(
+                controller: controller,
+                focusNode: focusNode,
+                style: Styles.searchText,
+                cursorColor: Styles.searchCursorColor,
+              ),
+            ),
+            GestureDetector(
+              onTap: controller.clear,
+              child: const Icon(CupertinoIcons.clear_thick_circled,
+                  color: Styles.searchIconColor),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
