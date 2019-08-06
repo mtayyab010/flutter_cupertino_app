7410041db2d5c698fc7b12f882954a44117f1a89
hiyongrui <172808H@mymail.nyp.edu.sg>
2019-08-06 20:37:41 +0800

    add date picker

--- lib/shopping_cart_tab.dart
+++ lib/shopping_cart_tab.dart
@@ -3,6 +3,11 @@ import 'package:provider/provider.dart';
 
 import 'model/app_state_model.dart';
 
+import 'package:intl/intl.dart';
+import 'styles.dart';
+
+const double _kDateTimePickerHeight = 216;
+
 //stateful widget, maintains list of purchases and customer's info
 class ShoppingCartTab extends StatefulWidget {
   @override
@@ -43,6 +48,50 @@ class _ShoppingCartTabState extends State<ShoppingCartTab> {
           name = newName;
         });
       },
+    );
+  }
+
+  Widget _buildDateAndTimePicker(BuildContext context) {
+    // NEW FROM HERE
+    return Column(
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: const <Widget>[
+                Icon(
+                  CupertinoIcons.clock,
+                  color: CupertinoColors.lightBackgroundGray,
+                  size: 28,
+                ),
+                SizedBox(width: 6),
+                Text(
+                  'Delivery time',
+                  style: Styles.deliveryTimeLabel,
+                ),
+              ],
+            ),
+            Text(
+              DateFormat.yMMMd().add_jm().format(dateTime),
+              style: Styles.deliveryTime,
+            ),
+          ],
+        ),
+        Container(
+          height: _kDateTimePickerHeight,
+          child: CupertinoDatePicker(
+            mode: CupertinoDatePickerMode.dateAndTime,
+            initialDateTime: dateTime,
+            onDateTimeChanged: (newDateTime) {
+              setState(() {
+                dateTime = newDateTime;
+              });
+            },
+          ),
+        ),
+      ],
     );
   }
 
@@ -111,8 +160,13 @@ class _ShoppingCartTabState extends State<ShoppingCartTab> {
             padding: const EdgeInsets.symmetric(horizontal: 16),
             child: _buildLocationField(),
           );
+        case 3:
+          return Padding(
+            padding: const EdgeInsets.fromLTRB(16, 8, 16, 24),
+            child: _buildDateAndTimePicker(context),
+          );
         default:
-        //Do nothing
+        //Do nothing. For now.
       }
       return null;
     });
